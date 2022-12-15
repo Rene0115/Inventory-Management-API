827a5f0824a637818d3b35861afcc18a81713e79
Rene0115 <enere0115@gmail.com>
2022-12-15 22:48:15 +0100

    validator setup

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -2,11 +2,9 @@
 /* eslint-disable import/extensions */
 import express from 'express';
 import userRouter from './user.routes.js';
-import postRouter from './post.routes.js';
 
 const router = express.Router();
 
 router.use('/users', userRouter);
-router.use('/posts', postRouter);
 
 export default router;
