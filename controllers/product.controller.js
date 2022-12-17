8ba54626f39b118a7f225a5e6f04d8c8bd9760f2
Rene0115 <enere0115@gmail.com>
2022-12-17 01:35:22 +0100

    create product feature setup

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,6 +1,32 @@
-import productService from "../services/product.service";
+/* eslint-disable class-methods-use-this */
+import _ from 'lodash';
+import productService from '../services/product.service';
 
 class ProductController {
-
+  async create(req, res) {
+    const data = {
+      name: req.body.name,
+      price: req.body.price,
+      category: req.body.category
+    };
+    if (!data.name || !data.price || !data.category) {
+      return res.status(404).send({
+        success: false,
+        message: 'must provide a name, price and category.'
+      });
+    }
+    const post = productService.create(data);
+    if (!post) {
+      return res.status(404).send({
+        success: false,
+        message: 'product was not created'
+      });
+    }
+    return res.status(200).send({
+      success: true,
+      message: 'product was successfully created.',
+      data: post
+    });
+  }
 }
-export de
+export default new ProductController();
